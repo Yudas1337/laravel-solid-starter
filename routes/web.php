3e4ec3f04b8cbb09c84dcb7e45aa2078ff69d143
Abdul kader <yudasmalabi@gmail.com>
2024-06-20 10:52:58 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Dashboard\ArticleController;
+use App\Http\Controllers\Dashboard\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories', CategoryController::class);
+Route::resource('articles', ArticleController::class);
